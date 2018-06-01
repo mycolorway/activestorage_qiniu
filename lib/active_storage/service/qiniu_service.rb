f31ad1188cf262c19626edf525106816b40a4e89
seandong <sindon@gmail.com>
2018-06-01 09:52:29 +0800

    CHG: remove expires_in

--- lib/active_storage/service/qiniu_service.rb
+++ lib/active_storage/service/qiniu_service.rb
@@ -93,7 +93,7 @@ module ActiveStorage
 
     def download_chunk(key, range)
       instrument :download_chunk, key: key, range: range do
-        uri = URI(url(key, expires_in: 30.seconds, disposition: :attachment))
+        uri = URI(url(key, disposition: :attachment))
         Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |client|
           client.get(uri, 'Range' => "bytes=#{range.begin}-#{range.exclude_end? ? range.end - 1 : range.end}").body
         end
